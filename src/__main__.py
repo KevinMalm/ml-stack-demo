bf79bb1cfddbdd3fcc5deb120f5db8a538e97268
KevinMalm <33231167+KevinMalm@users.noreply.github.com>
2025-08-01 08:10:05 -0500

    Initial commit of refactored code

--- src/__main__.py
+++ src/__main__.py
@@ -1,30 +1,14 @@
 import os
-import mlflow
 import json
+import mlflow
+import ml_stack
 
 
 def main():
-    mlflow_configure()
+    ml_stack.configure()
 
     with mlflow.start_run():
         pass
-
-
-def mlflow_configure():
-    # Load Env Vars
-    _experiment = os.getenv("MLFLOW_EXPERIMENT_NAME")
-    _tags = os.getenv("MLFLOW_EXPERIMENT_TAGS")
-    # Validate
-    if _experiment is None or _tags is None:
-        raise Exception("Environment has not been setup correctly.")
-    # Parse the ML Flow TAGs
-    tags = json.loads(_tags.replace("\\", ""))
-
-    # Set the Experiment name
-    mlflow.set_experiment(_experiment)
-    # Set the Experiment tags
-    for x in tags:
-        mlflow.set_tag(x["key"], x["value"])
 
 
 if __name__ == "__main__":
