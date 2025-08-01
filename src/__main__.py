6c6965f1ee0f0d631d6e152824bdeeb9c0339f38
KevinMalm <33231167+KevinMalm@users.noreply.github.com>
2025-08-01 08:37:29 -0500

    Add testing API for Demo

---
+++ src/__main__.py
@@ -0,0 +1,44 @@
+import os
+from flask import Flask, jsonify
+import random
+import string
+from breeds import BREEDS
+
+app = Flask(__name__)
+
+
+def generate_record():
+    is_dog = random.random() < 0.4
+    my_str = (
+        random.choice(BREEDS)
+        if is_dog
+        else "".join(
+            random.choices(
+                string.ascii_letters + string.digits, k=random.randint(4, 34)
+            )
+        )
+    )
+
+    ascii_arr = [ord(char) for char in my_str]
+
+    return ascii_arr, my_str, is_dog
+
+
+@app.route("/live", methods=["GET"])
+def get_next():
+    ascii_string, _truth_string, _is_dog = generate_record()
+    response = {"content": ascii_string}
+
+    return jsonify(response)
+
+
+@app.route("/test", methods=["GET"])
+def get_next():
+    ascii_string, truth_string, is_dog = generate_record()
+    response = {"content": ascii_string, "value": truth_string, "flag": is_dog}
+
+    return jsonify(response)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=3000)
